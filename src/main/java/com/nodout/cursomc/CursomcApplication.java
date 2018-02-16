0cd56bf5b5f15dc95939378b67d469c4df543e76
MiltonMatias22 <nodout.develops@gmail.com>
2018-02-16 10:31:37 -0200

    Crindo operação de instanciação - Categoria

--- src/main/java/com/nodout/cursomc/CursomcApplication.java
+++ src/main/java/com/nodout/cursomc/CursomcApplication.java
@@ -1,12 +1,32 @@
 package com.nodout.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nodout.cursomc.domain.Categoria;
+import com.nodout.cursomc.repositoreis.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		Categoria categoria1 = new Categoria(null, "INFORMÁTICA");
+		Categoria categoria2 = new Categoria(null, "ESCRITÓRIO");
+		
+		categoriaRepository.save(Arrays.asList(categoria1, categoria2));
+	}
+	
 }
